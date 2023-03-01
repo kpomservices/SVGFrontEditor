90b47f5449feef7ccd8e0a2f6c8287725e1ecd0c
kpomservices <kpomservices@gmail.com>
2023-03-01 16:56:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -9,15 +9,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </div>
   );
 }
