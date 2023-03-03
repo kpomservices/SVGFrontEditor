fd6452a4b2ef491b64f6826f7811d6128a6e23d4
kpomservices <kpomservices@gmail.com>
2023-03-03 16:26:57 +0530

    UI design changes done.

--- src/App.js
+++ src/App.js
@@ -25,13 +25,13 @@ function App() {
 			  <Button2Style>layout03</Button2Style>
 			</div>
 		  </div>
-		  <div className="border rounded-sm border-blue-700 px-3 pt-2 pb-3 my-2">
+		  {/*<div className="border rounded-sm border-blue-700 px-3 pt-2 pb-3 my-2">
 			<BoxHeading>Delete parts inside SVG</BoxHeading>
 			<p className="font-sans antialiased font-semibold text-red-700"><input type="checkbox" /> Delete mode</p>
 			<div className="btns">
 			  <button className="bg-gray-400 text-white px-4 pt-1.5 pb-2 rounded mx-1 my-3">Delete selected logo parts</button>
 			</div>
-		  </div>
+		  </div>*/}
 		  <div className="border rounded-sm border-blue-700 px-3 pt-2 pb-3 my-2">
 			<BoxHeading>General</BoxHeading>
 			<p className="font-sans antialiased font-semibold">Background color:</p>
@@ -41,7 +41,7 @@ function App() {
 		  </div>
 		</div>
 		<div className="rightSide md:w-1/2 px-2 w-full">
-		  <div className="border rounded-sm border-blue-700 px-3 pt-2 pb-3 my-2">
+		  {/*<div className="border rounded-sm border-blue-700 px-3 pt-2 pb-3 my-2">
 			<BoxHeading>Logo settings</BoxHeading>
 			<div className="sm:flex">
 			  <div className="sm:w-1/2 sm:px-2 w-full pt-3 sm:pt-0">
@@ -58,13 +58,13 @@ function App() {
 			  <ButtonStyle><ArrowRightIcon className="h-5 w-5" /></ButtonStyle>
 			  </div>
 			</div>
-		  </div>
+		  </div>*/}
 		  <div className="border rounded-sm border-blue-700 px-3 pt-2 pb-3 my-2">
 			<BoxHeading>Brand Name</BoxHeading>
-			<div className="flex pt-3">
+			{/*<div className="flex pt-3">
 			<input type="text" placeholder="Brand Name" className="w-full pt-1.5 pb-2 px-3 font-sans antialiased border border-black rounded-l-md focus:outline-none border-r-0" />
 			<button className="bg-blue-700 text-white px-4 py-1.5 rounded-r-md">Update</button>
-			</div>
+			</div>*/}
 			<hr className="my-2.5" />
 			<div className="sm:flex">
 			  <div className="sm:w-1/2 sm:px-2 w-full">
@@ -86,24 +86,24 @@ function App() {
 			  <span>120</span>
 			  </div>
 			</div>
-			<div className="sm:w-1/2 sm:px-2 w-full pt-3 sm:pt-0">
+			{/*<div className="sm:w-1/2 sm:px-2 w-full pt-3 sm:pt-0">
 			  <p className="font-sans antialiased font-semibold">Brandname move:</p>
 			  <p>You can also drag items</p>
 			  <ButtonStyle><ArrowUpIcon className="h-5 w-5" /></ButtonStyle>
 			  <ButtonStyle><ArrowDownIcon className="h-5 w-5" /></ButtonStyle>
 			  <ButtonStyle><ArrowLeftIcon className="h-5 w-5" /></ButtonStyle>
 			  <ButtonStyle><ArrowRightIcon className="h-5 w-5" /></ButtonStyle>
-			</div>
+			</div>*/}
 			</div>
 		  </div>
 		  <div className="border rounded-sm border-blue-700 px-3 pt-2 pb-3 my-2">
 			<BoxHeading>Tagline</BoxHeading>
-			<p className="font-sans antialiased font-semibold"><input type="checkbox" /> Show tagline</p>
+			{/*<p className="font-sans antialiased font-semibold"><input type="checkbox" /> Show tagline</p>
 			<hr className="my-2.5" />
 			<div className="flex">
 			<input type="text" placeholder="Tagline text" className="w-full pt-1.5 pb-2 px-3 font-sans antialiased border border-black rounded-l-md focus:outline-none border-r-0" />
 			<button className="bg-blue-700 text-white px-4 py-1.5 rounded-r-md">Update</button>
-			</div>
+			</div>*/}
 			<hr className="my-2.5" />
 			<div className="sm:flex">
 			  <div className="sm:w-1/2 sm:px-2 w-full">
@@ -112,7 +112,8 @@ function App() {
 			</div>
 			<div className="sm:w-1/2 sm:px-2 w-full">
 			  <p className="font-sans antialiased font-semibold">Tagline Font color:</p>
-			  <Button2Style>#000000</Button2Style>
+			  {/*<Button2Style>#000000</Button2Style>*/}
+			  <Button2Style><span className="p-1.5 rounded-full bg-black shadow shadow-black leading-none inline-block mr-0.5 ml-0.5"></span> #000000</Button2Style>
 			</div>
 			</div>
 			<hr className="my-2.5" />
@@ -125,14 +126,14 @@ function App() {
 			  <span>40</span>
 			  </div>
 			</div>
-			<div className="sm:w-1/2 sm:px-2 w-full pt-3 sm:pt-0">
+			{/*<div className="sm:w-1/2 sm:px-2 w-full pt-3 sm:pt-0">
 			  <p className="font-sans antialiased font-semibold">Tagline move:</p>
 			  <p className="font-sans text-sm antialiased">You can also drag items</p>
 			  <ButtonStyle><ArrowUpIcon className="h-5 w-5" /></ButtonStyle>
 			  <ButtonStyle><ArrowDownIcon className="h-5 w-5" /></ButtonStyle>
 			  <ButtonStyle><ArrowLeftIcon className="h-5 w-5" /></ButtonStyle>
 			  <ButtonStyle><ArrowRightIcon className="h-5 w-5" /></ButtonStyle>
-			</div>
+			</div>*/}
 			</div>
 		  </div>
 		</div>
